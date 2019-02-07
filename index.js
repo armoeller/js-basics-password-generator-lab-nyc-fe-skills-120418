de9171227eb84701a3ea5f5571eb07ed21198446
Andy Moeller <ar.moeller1@gmail.com>
2019-02-07 23:11:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 window.addEventListener("load", () => {
   //start your code here
+  const lowerCaseLetter = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
+  
+  const button = document.getElementById('my-button')
+  const output = document.querySelector('output')
+  
+  const onSubmit = () => {
+  const input = document.getElementById('textArea')
+  const whatTheyTyped = input.value
+  }
+  
+  button.addEventListener("click", onSubmit)
 });
